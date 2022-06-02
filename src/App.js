4955713e250ab1e5c75c6afa9503ffc35e55d19f
Jimmy Thomas <jthomas302@gmail.com>
2022-06-02 03:17:52 -0400

    remove hardcoded consts from ExpenseItem.js and replace with props objects initialized in App.js - passing from parent to child

--- src/App.js
+++ src/App.js
@@ -1,10 +1,52 @@
 import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
+  // props
+  const expenses = [
+    {
+      id: 1,
+      title: "Car Insurance",
+      amount: 191.67,
+      date: new Date(2022, 5, 23),
+    },
+    { id: 2, title: "Phone Bill", amount: 131.23, date: new Date(2022, 5, 14) },
+    {
+      id: 3,
+      title: "Car Payment",
+      amount: 528.99,
+      date: new Date(2022, 5, 15),
+    },
+    {
+      id: 4,
+      title: "Rent",
+      amount: 1600,
+      date: new Date(2022, 5, 1),
+    },
+  ];
+
   return (
     <div>
       <h1> Let's Get Started </h1>
-      <ExpenseItem />
+      <ExpenseItem
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+        date={expenses[0].date}
+      />
+      <ExpenseItem
+        title={expenses[1].title}
+        amount={expenses[1].amount}
+        date={expenses[1].date}
+      />
+      <ExpenseItem
+        title={expenses[2].title}
+        amount={expenses[2].amount}
+        date={expenses[2].date}
+      />
+      <ExpenseItem
+        title={expenses[3].title}
+        amount={expenses[3].amount}
+        date={expenses[3].date}
+      />
     </div>
   );
 }
