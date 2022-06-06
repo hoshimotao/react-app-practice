34ab015f5d3e6a426604623e53b75d2e99887fcb
Jimmy Thomas <jthomas302@gmail.com>
2022-06-06 09:23:49 -0400

    add simple state object for title

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,16 +1,26 @@
+import { useState } from "react";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
+  const [title, setTitle] = useState(props.title);
+  // title is original state - setTitle is function to update title state
+
+  const clickHandler = () => {
+    // on click fires setTitle, passing argument with new data updates title
+    setTitle("New Title");
+    console.log(title);
+  };
   // parent component = Expenses.js
   return (
     <Card className="expense-item">
       <ExpenseDate expenseDate={props.itemDate} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price"> ${props.amount} </div>
       </div>
+      <button onClick={clickHandler}> Change Title </button>
     </Card>
   );
 }
